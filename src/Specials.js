97c271b18769453e108d17aee3e795b1c7972848
Rodrigo <rodrigo.rpereira1998@gmail.com>
2023-09-30 14:02:14 +0100

    finish specials section and make website resposive for current components

--- src/Specials.js
+++ src/Specials.js
@@ -1,13 +1,77 @@
 import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper/modules";
+import { Pagination } from "swiper/modules";
 // Import Swiper styles
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "swiper/css/scrollbar";
 
-const dishes = [];
+import LemonDessert from "./assets/lemon dessert.jpg";
+import GreekSalad from "./assets/greek salad.jpg";
+import Bruchetta from "./assets/brucheta.png";
+
+const dishes = [
+  {
+    name: "Greek salad",
+    price: "5.99",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi",
+    image: GreekSalad,
+  },
+  {
+    name: "Lemon dessert",
+    price: "5.99",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi",
+    image: LemonDessert,
+  },
+  {
+    name: "Bruchetta",
+    price: "5.99",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi",
+    image: Bruchetta,
+  },
+  {
+    name: "Greek salad",
+    price: "5.99",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi",
+    image: GreekSalad,
+  },
+  {
+    name: "Greek salad",
+    price: "5.99",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi",
+    image: GreekSalad,
+  },
+  {
+    name: "Greek salad",
+    price: "5.99",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi",
+    image: GreekSalad,
+  },
+];
 
 export function Specials() {
+  const listItems = dishes.map((dish) => {
+    return (
+      <SwiperSlide>
+        <div className="dish-card">
+          <img src={dish.image} />
+          <div className="title-area">
+            <h3>{dish.name}</h3>
+            <p className="price">{dish.price}</p>
+          </div>
+          <p className="description">{dish.description}</p>
+        </div>
+      </SwiperSlide>
+    );
+  });
+
   return (
     <div className="specials">
       <div className="title-area">
@@ -16,24 +80,30 @@ export function Specials() {
       </div>
       <div className="swiper-container">
         <Swiper
-          spaceBetween={50}
-          slidesPerView={3}
-          onSlideChange={() => console.log("slide change")}
-          onSwiper={(swiper) => console.log(swiper)}
+          pagination={{
+            clickable: true,
+          }}
+          breakpoints={{
+            0: {
+              slidesPerView: 1,
+              spaceBetween: 100,
+              centeredSlides: true,
+            },
+            700: {
+              slidesPerView: 2,
+              spaceBetween: 100,
+              centeredSlides: true,
+            },
+            1085: {
+              slidesPerView: 3,
+              spaceBetween: 110,
+              centeredSlides: false,
+            },
+          }}
+          modules={[Pagination]}
+          className="mySwiper"
         >
-          <SwiperSlide>
-            <div className="dish-card">Slide 1</div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className="dish-card">Slide 2</div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className="dish-card">Slide 3</div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className="dish-card">Slide 4</div>
-          </SwiperSlide>
-          ...
+          {listItems}
         </Swiper>
       </div>
     </div>
